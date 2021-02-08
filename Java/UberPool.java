28c8096052a4a7ec42711d3bc958704a7aad09cd
Erick Eduardo <edu.2010@live.com.mx>
2021-02-08 03:40:28 -0600

    Se crean las clases para herencia en Java con aparentes errores en ArrayList

--- Java/UberPool.java
+++ Java/UberPool.java
@@ -1,3 +1,10 @@
-public class UberPool {
+public class UberPool extends Car {
+    String brand;
+    String model;
 
+    public UberPool(String license, Account driver, String brand, String model) {
+        super(license, driver);
+        this.brand = brand;
+        this.model = model;
+    }
 }
