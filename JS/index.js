c70d5acba91a6ec20e0bf2b82868e7719124e6df
Erick Eduardo <edu.2010@live.com.mx>
2021-02-10 08:52:17 -0600

    Ya funcionan las clases en Python y JS x2

--- JS/index.js
+++ JS/index.js
@@ -1,6 +1,6 @@
 var car = new Car("MAR123", new Account("Marie", "CEDMAR"))
-car.passenger = 4
-car.printDataCar()
+car.passenger = 4;
+car.printDataCar();
 
 var uberX = new UberX("LIO123", new Account("Lionel", "LIO123"), "Mercedes", "CL500")
 uberX.passenger = 4;
